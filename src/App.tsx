81543679e4e5e6e6f27b365a2b232d56c0e72a21
hseager <harvey.seager@googlemail.com>
2024-03-28 21:35:22 +0000

    Adding wasm stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import './App.css'
+import { ChangeEvent, useMemo } from "react";
+import "./App.css";
+import * as wasmPkg from "./pkg";
 
 function App() {
+  const wasm = useMemo(() => wasmPkg.create(), []);
+
+  const handleInput = (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const input = formData.get("input");
+
+    console.log(input);
+
+    // wasm.run();
+  };
 
   return (
     <>
       <h1>You are Merlin</h1>
       <p>A text adventure game built with Rust and WASM.</p>
-      <textarea className="input" cols={20} rows={16} readOnly/>
-      <fieldset>
-        <input className="input" type="text" /><button>Submit</button>
-      </fieldset>
+      <textarea
+        className="input"
+        cols={20}
+        rows={16}
+        value={"Some text"}
+        readOnly
+      />
+      <form onSubmit={handleInput}>
+        <fieldset>
+          <input className="input" name="input" type="text" />
+          <button type="submit">Submit</button>
+        </fieldset>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import wasm from "vite-plugin-wasm";
+// import topLevelAwait from "vite-plugin-top-level-await";
+
+// TODO topLevelAwait() not working with bun
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), wasm()],
+});
