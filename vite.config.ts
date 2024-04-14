a585f2282b5b002b75a5f01b2407dc29273c0572
hseager <harvey.seager@googlemail.com>
2024-04-14 22:01:14 +0100

    Fix prod build

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,7 @@ import wasm from "vite-plugin-wasm";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), wasm()],
+  build: {
+    target: "esnext", //browsers can handle the latest ES features
+  },
 });
